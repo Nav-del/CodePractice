fee2fcda9bef8a10db5f5116daf3e8de990a2fea
Navtej Chakravarty <navarette0705@gmail.com>
2026-04-30 17:32:48 +0530

    Implement majority element algorithm using HashMap

---
+++ 169. Majority Element.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for(int i=0; i < nums.length; i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i], map.get(nums[i]) + 1);
+            }
+            else{
+                map.put(nums[i], 1);
+            }
+        }
+
+        int m = nums.length/2;
+        int result =0;
+
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+            if(entry.getValue() > m){
+                result = entry.getKey();
+            }
+        }
+        return result;
+    }
+}
+
+//HASH MAPS IMP
